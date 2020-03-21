4c20ef47c44d6c75247f50ebe277541f29eb94e1
Ricky Guzman <r.guzman@utexas.edu>
2020-03-21 15:25:11 -0500

    messing around with pandas and sklearn

--- houseprices.py
+++ houseprices.py
@@ -1,7 +1,19 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 
+df = pd.read_csv (r'train.csv')
+x = df['YearBuilt'].values.reshape(-1,1)
+#x = df[['YearBuilt','OverallQual']].values.reshape(-1,1)
+y = df['SalePrice'].values.reshape(-1,1)
+linear_regressor = LinearRegression()
+linear_regressor.fit(x,y)
+y_predict = linear_regressor.predict(x)
 
-df = pd.read_csv (r'test.csv')
-print (df)
+plt.scatter(x,y)
+plt.plot(x,y_predict,color = 'blue')
+plt.show()
+
+
